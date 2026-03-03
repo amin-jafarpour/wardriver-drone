98a00d2276acc66c9c9aa74433daccc665def5d0
amin-jafarpour <a.jafarpour2001@gmail.com>
2026-03-03 15:58:37 -0800

    deleted scanner.c file in app project directory

--- src/app/main/sdcard_main.c
+++ src/app/main/sdcard_main.c
@@ -6,7 +6,16 @@
 #include "driver/sdmmc_host.h"
 #include "driver/gpio.h"
 
-#include "scanner.c"
+#include <string.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/event_groups.h"
+#include "esp_wifi.h"
+#include "esp_log.h"
+#include "esp_event.h"
+#include "nvs_flash.h"
+#include "regex.h"
+
+
 
 #if SOC_SDMMC_IO_POWER_EXTERNAL
 #include "sd_pwr_ctrl_by_on_chip_ldo.h"
@@ -56,38 +65,158 @@ pin_configuration_t config = {
 };
 #endif //CONFIG_DEBUG_PIN_CONNECTIONS
 
-static esp_err_t overwrite_file(const char *path, char *data)
-{
-    ESP_LOGI(TAG, "Opening file %s", path);
-    FILE *file = fopen(path, "w");
-    if (file == NULL) {
-        ESP_LOGE(TAG, "Failed to open file for writing");
-        return ESP_FAIL;
+
+
+
+
+
+
+#define DEFAULT_SCAN_LIST_SIZE CONFIG_SCAN_LIST_SIZE
+
+#ifdef CONFIG_USE_SCAN_CHANNEL_BITMAP
+#define CHANNEL_LIST_SIZE 3
+static uint8_t channel_list[CHANNEL_LIST_SIZE] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14}; // {1, 6, 11};
+#endif 
+
+
+extern const char *TAG;
+
+#ifdef CONFIG_USE_SCAN_CHANNEL_BITMAP
+static void array_2_channel_bitmap(const uint8_t channel_list[], const uint8_t channel_list_size, wifi_scan_config_t *scan_config) {
+
+    for(uint8_t i = 0; i < channel_list_size; i++) {
+        uint8_t channel = channel_list[i];
+        scan_config->channel_bitmap.ghz_2_channels |= (1 << channel);
     }
-    fprintf(file, data);
-    fclose(file);
-    ESP_LOGI(TAG, "File written");
-    return ESP_OK;
+}
+#endif
+
+void wifi_scan(sdmmc_card_t *card)
+{
+    esp_err_t ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+        ESP_ERROR_CHECK(nvs_flash_erase());
+        ret = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK( ret );
+
+    ESP_ERROR_CHECK(esp_netif_init());
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+    esp_netif_t *sta_netif = esp_netif_create_default_wifi_sta();
+    assert(sta_netif);
+
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+
+    uint16_t number = DEFAULT_SCAN_LIST_SIZE;
+    wifi_ap_record_t ap_info[DEFAULT_SCAN_LIST_SIZE];
+    uint16_t ap_count = 0;
+    memset(ap_info, 0, sizeof(ap_info));
+
+
+    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
+    ESP_ERROR_CHECK(esp_wifi_start());
+
+
+
+
+
+
+
+
+
+ 
+  
+    // char data[DATA_SIZE];
+    // snprintf(data, DATA_SIZE, "%s %s!\n", "Hello", card->cid.name);
+    // ret = overwrite_file(file_hello, data);
+    // if (ret != ESP_OK) {
+    //     return;
+    // }
+
+
+
+    
+  
+  const char *file_path = "/wardriver.txt";
+    while(true)
+    {
+    FILE *f = fopen(file_path, "a");
+    if (f == NULL) {
+    perror("fopen");
+    return;
+    }
+
+    
+        #ifdef CONFIG_USE_SCAN_CHANNEL_BITMAP
+            wifi_scan_config_t *scan_config = (wifi_scan_config_t *)calloc(1,sizeof(wifi_scan_config_t));
+            if (!scan_config) {
+                ESP_LOGE(TAG, "Memory Allocation for scan config failed!");
+                return;
+            }
+            array_2_channel_bitmap(channel_list, CHANNEL_LIST_SIZE, scan_config);
+            esp_wifi_scan_start(scan_config, true);
+            free(scan_config);
+
+        #else
+            esp_wifi_scan_start(NULL, true);
+        #endif
+
+            ESP_LOGI(TAG, "Max AP number ap_info can hold = %u", number);
+            ESP_ERROR_CHECK(esp_wifi_scan_get_ap_num(&ap_count));
+            ESP_ERROR_CHECK(esp_wifi_scan_get_ap_records(&number, ap_info));
+            ESP_LOGI(TAG, "Total APs scanned = %u, actual AP number ap_info holds = %u", ap_count, number);
+            for (int i = 0; i < number; i++) {
+                fprintf(f, "Hello log\n");
+                ESP_LOGI(TAG, "hi");
+            }
+        vTaskDelay(pdMS_TO_TICKS(500));
+        fclose(f);
+    }
+
+
 }
 
-static esp_err_t read_file(const char *path)
-{
-    ESP_LOGI(TAG, "Reading file %s", path);
-    FILE *file = fopen(path, "r");
-    if (file == NULL) {
-        ESP_LOGE(TAG, "Failed to open file for reading");
-        return ESP_FAIL;
-    }
-    char line[DATA_SIZE];
-    fgets(line, sizeof(line), file);
-    fclose(file);
-    char *pos = strchr(line, '\n');
-    if (pos) {
-        *pos = '\0';
-    }
-    ESP_LOGI(TAG, "Read from file: '%s'", line);
-    return ESP_OK;
-}
+
+
+
+
+
+// static esp_err_t overwrite_file(const char *path, char *data)
+// {
+//     ESP_LOGI(TAG, "Opening file %s", path);
+//     FILE *file = fopen(path, "w");
+//     if (file == NULL) {
+//         ESP_LOGE(TAG, "Failed to open file for writing");
+//         return ESP_FAIL;
+//     }
+//     fprintf(file, data);
+//     fclose(file);
+//     ESP_LOGI(TAG, "File written");
+//     return ESP_OK;
+// }
+
+
+
+
+// static esp_err_t read_file(const char *path)
+// {
+//     ESP_LOGI(TAG, "Reading file %s", path);
+//     FILE *file = fopen(path, "r");
+//     if (file == NULL) {
+//         ESP_LOGE(TAG, "Failed to open file for reading");
+//         return ESP_FAIL;
+//     }
+//     char line[DATA_SIZE];
+//     fgets(line, sizeof(line), file);
+//     fclose(file);
+//     char *pos = strchr(line, '\n');
+//     if (pos) {
+//         *pos = '\0';
+//     }
+//     ESP_LOGI(TAG, "Read from file: '%s'", line);
+//     return ESP_OK;
+// }
 
 #if CONFIG_PIN_CARD_POWER_RESET
 static esp_err_t reset_card_power(void)
@@ -219,23 +348,11 @@ void sdcard_setup(sdcard_callback callback)
     callback(card);
 }
 
-void dummy(sdmmc_card_t *card)
-{
-    wifi_scan();
-    esp_err_t ret;
-    const char *file_hello = MOUNT_POINT"/hello.txt";
-    char data[DATA_SIZE];
-    snprintf(data, DATA_SIZE, "%s %s!\n", "Hello", card->cid.name);
-    ret = overwrite_file(file_hello, data);
-    if (ret != ESP_OK) {
-        return;
-    }
-}
 
 void app_main(void)
 {
 
-    sdcard_setup(dummy);
+    sdcard_setup(wifi_scan);
     // POXIS
     // const char *file_hello = MOUNT_POINT"/hello.txt";
     // char data[DATA_SIZE];
