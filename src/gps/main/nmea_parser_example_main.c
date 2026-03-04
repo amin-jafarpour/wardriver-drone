2f6d510840c7b519e1d4fcd31287b23f17b1b48d
amin-jafarpour <a.jafarpour2001@gmail.com>
2026-03-03 17:19:17 -0800

    modified GPS app

--- src/gps/main/nmea_parser_example_main.c
+++ src/gps/main/nmea_parser_example_main.c
@@ -1,12 +1,3 @@
-/* NMEA Parser example, that decode data stream from GPS receiver
-
-   This example code is in the Public Domain (or CC0 licensed, at your option.)
-
-   Unless required by applicable law or agreed to in writing, this
-   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-   CONDITIONS OF ANY KIND, either express or implied.
-*/
-
 #include <stdio.h>
 #include <stdlib.h>
 #include "freertos/FreeRTOS.h"
@@ -14,9 +5,10 @@
 #include "esp_log.h"
 #include "nmea_parser.h"
 
-static const char *TAG = "gps_demo";
+extern const char *TAG;
 
-#define TIME_ZONE (+8)   //Beijing Time
+// #define TIME_ZONE (+8)   //Beijing Time
+#define TIME_ZONE (-8)   //Vancouver Time
 #define YEAR_BASE (2000) //date in GPS starts from 2000
 
 /**
