46fdbf2d1d95b2dfae0ad5e5daf35b3862ab8b1c
Amin Jafarpour <a.jafarpour2001@gmail.com>
2026-03-12 20:54:58 -0700

    added get_inodes() function in sdcard.py

--- webapp/sdcard.py
+++ webapp/sdcard.py
@@ -2,6 +2,7 @@
     $ pip install psutil
 """
 import psutil
+import os
 
 def get_partitions():
     partitions = [
@@ -12,3 +13,12 @@ def get_partitions():
     for p in partitions:
         partition_list.append({'Partition': p.device, 'Mountpoint': p.mountpoint, 'Filesystem': p.fstype}) 
     return partition_list
+
+def get_inodes(mountpoint):
+    directory_list = []
+    file_list = []
+    for root, directories, files in os.walk(mountpoint):
+        directory_list.append([os.path.join(root, directory) for directory in directories])
+        file_list.append([os.path.join(root, file) for file in files])
+    return (directory_list, file_list)
+
