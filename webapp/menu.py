a33f0a24c60f3e301b1839243de42fb6e8101b20
Amin Jafarpour <a.jafarpour2001@gmail.com>
2026-03-24 11:18:39 -0700

    ...

--- webapp/menu.py
+++ webapp/menu.py
@@ -34,30 +34,32 @@ def extraction():
     if 'ap_str_list' in st.session_state:
         record_list = []
         for ap_str in st.session_state.ap_str_list:
-            record = ap.WifiAPRecord(ap_str[0], ap_str[1], ap_str[2], ap_str[3], ap_str[4], ap_str[5], ap_str[6])
-            record.ssid = ap_str[7]
-            record.primary_channel = ap_str[8]
-            record.second_channel = ap_str[9]
-            record.rssi = ap_str[10]
-            record.authmode = ap_str[11]
-            record.pairwise_cipher = ap_str[12]
-            record.group_cipher = ap_str[13]
-            record._ant = ap_str[14]
-            record.country_code = ap_str[15]
-            record.country_start_channel = ap_str[16]
-            record.country_end_channel = ap_str[17]
-            record.max_tx_power = ap_str[18]
-            record.country_policy = ap_str[19]
-            record.wifi_AP_HE = ap_str[20]
-            record.bss_color = ap_str[21]
-            record.partial_bss_color = ap_str[22]
-            record.bss_color_disabled = ap_str[23]
-            record.bssid_index = ap_str[24]
-            record.bandwidth = ap_str[25]
-            record.vht_ch_freq1 = ap_str[26]
-            record.vht_ch_freq2 = ap_str[27]
-            record_list.append(record)
-
+            try:
+                record = ap.WifiAPRecord(ap_str[0], ap_str[1], ap_str[2], ap_str[3], ap_str[4], ap_str[5], ap_str[6])
+                record.ssid = ap_str[7]
+                record.primary_channel = ap_str[8]
+                record.second_channel = ap_str[9]
+                record.rssi = ap_str[10]
+                record.authmode = ap_str[11]
+                record.pairwise_cipher = ap_str[12]
+                record.group_cipher = ap_str[13]
+                record._ant = ap_str[14]
+                record.country_code = ap_str[15]
+                record.country_start_channel = ap_str[16]
+                record.country_end_channel = ap_str[17]
+                record.max_tx_power = ap_str[18]
+                record.country_policy = ap_str[19]
+                record.wifi_AP_HE = ap_str[20]
+                record.bss_color = ap_str[21]
+                record.partial_bss_color = ap_str[22]
+                record.bss_color_disabled = ap_str[23]
+                record.bssid_index = ap_str[24]
+                record.bandwidth = ap_str[25]
+                record.vht_ch_freq1 = ap_str[26]
+                record.vht_ch_freq2 = ap_str[27]
+                record_list.append(record)
+            except:
+                continue
         st.session_state.record_list = record_list
 
 
